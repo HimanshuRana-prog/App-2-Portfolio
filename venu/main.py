8688caa8bea3ffb81c33a9cefed20cd62ea04af7
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-10-11 12:39:31 +0530

    app completed

--- venu/main.py
+++ venu/main.py
@@ -25,14 +25,24 @@ Blow yo can find some of the apps I have built in python.feel free to contact me
 """
 st.write(content2)
 
-col3,col4 = st.columns(2)
+col3,empty_col ,col4 = st.columns([1.5,0.5,1.5])
 
 df = pandas.read_csv("data.csv",sep=";")
+
 with col3:
     for index,row in df.iterrows():
         st.header(row["title"])
+        st.write(row['description'])
+        st.image("image/"+ row["image"])
+        st.write("[Source Code]({row[https://pythonhow.com]})")
 
 
 with col4:
     for index,row in df.iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("image/"+ row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+
