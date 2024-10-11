f749b5d002d96aa44251d3d893f0a2945461c8bc
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-10-11 10:57:36 +0530

    Add descrption on thr new columns

--- venu/main.py
+++ venu/main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout = "wide")
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("image/file.png")
 
 with col2:
     st.title("Himanshu")
@@ -19,3 +19,8 @@ with col2:
     opportunities in the tech industry.
 """  
     st.info(content)
+
+content2 = """
+Blow yo can find some of the apps I have built in python.feel free to contact me!
+"""
+st.write(content2)
